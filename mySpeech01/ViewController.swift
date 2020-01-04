181fa6c0e04ece3b77ba8f7ea487a00ec1601e90
Huang Grace <huang.yahui.grace@gmail.com>
2020-01-04 15:05:18 +0800

    語音辨識授權

--- mySpeech01/ViewController.swift
+++ mySpeech01/ViewController.swift
@@ -7,14 +7,114 @@
 //
 
 import UIKit
+import Speech
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
 
+    @IBOutlet weak var textView: UITextField!
+    @IBOutlet weak var microphoneButton: UIButton!
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "zh-hant"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        microphoneButton.isEnabled = false  //2
+        speechRecognizer?.delegate = self  //3
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in  //4
+            var isButtonEnabled = false
+            switch authStatus {  //5
+            case .authorized:
+                isButtonEnabled = true
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            @unknown default:
+                print("unknown authStatus")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.microphoneButton.isEnabled = isButtonEnabled
+            }
+        }
     }
 
-
+    @IBAction func microphoneTapped(_ sender: AnyObject) {
+     
+    }
+    
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+//        let audioSession = AVAudioSession.sharedInstance()
+//        do {
+//            try audioSession.setCategory(AVAudioSession.Category.record)
+//            try audioSession.setMode(AVAudioSession.Mode.measurement)
+//            try audioSession.setActive(true, withFlags: .notifyOthersOnDeactivation)
+//        } catch {
+//            print("audioSession properties weren't set because of an error.")
+//        }
+//        
+//        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+//        
+//        guard let inputNode = audioEngine.inputNode else {
+//            fatalError("Audio engine has no input node")
+//        }
+//        
+//        guard let recognitionRequest = recognitionRequest else {
+//            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+//        }
+//        
+//        recognitionRequest.shouldReportPartialResults = true
+//        
+//        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+//            
+//            var isFinal = false
+//            
+//            if result != nil {
+//                
+//                self.textView.text = result?.bestTranscription.formattedString
+//                isFinal = (result?.isFinal)!
+//            }
+//            
+//            if error != nil || isFinal {
+//                self.audioEngine.stop()
+//                inputNode.removeTap(onBus: 0)
+//                
+//                self.recognitionRequest = nil
+//                self.recognitionTask = nil
+//                
+//                self.microphoneButton.isEnabled = true
+//            }
+//        })
+//        
+//        let recordingFormat = inputNode.outputFormat(forBus: 0)
+//        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+//            self.recognitionRequest?.append(buffer)
+//        }
+//        
+//        audioEngine.prepare()
+//        
+//        do {
+//            try audioEngine.start()
+//        } catch {
+//            print("audioEngine couldn't start because of an error.")
+//        }
+//        
+//        textView.text = "Say something, I'm listening!"
+        
+    }
 }
 
