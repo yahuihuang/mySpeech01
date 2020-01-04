01f17a5da28b99bf832464a81ec9f9e39e57b2a2
Huang Grace <huang.yahui.grace@gmail.com>
2020-01-04 14:36:49 +0800

    Initial Commit

---
+++ mySpeech01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mySpeech01
+//
+//  Created by grace on 2020/1/4.
+//  Copyright © 2020 grace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
